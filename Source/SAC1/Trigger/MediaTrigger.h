f1c5996d89e7a5585420aa616bcd995badafc1e2
WI-Jeong <cpdls23@naver.com>
2023-11-13 10:57:37 +0900

    마이그레이션(다른 프로젝트 에셋 가져오기)

---
+++ Source/SAC1/Trigger/MediaTrigger.h
@@ -0,0 +1,20 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "../GameInfo.h"
+#include "Trigger.h"
+#include "MediaTrigger.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class SAC1_API AMediaTrigger : public ATrigger
+{
+	GENERATED_BODY()
+
+public:
+	AMediaTrigger();
+	
+};
