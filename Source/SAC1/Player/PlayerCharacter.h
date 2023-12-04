e4b103a58a57359d071acb26552aa25e6ea6b603
WI-Jeong <cpdls23@naver.com>
2023-12-04 10:40:15 +0900

    플레이어캐릭터 헤더파일

--- Source/SAC1/Player/PlayerCharacter.h
+++ Source/SAC1/Player/PlayerCharacter.h
@@ -41,18 +41,22 @@ protected:
 	ETeam	mTeam;
 
 public:
-	ETeam GetTeam()
+	ETeam GetTeam() 
+	//GetTeam()는 ETeam 타입의 객체를 반환하는 공개 함수
 	{
 		return mTeam;
+		//mTeam이라는 멤버 변수의 값을 가져오는 것
 	}
 
 	void SetTeam(ETeam Team)
+	//SetTeam(ETeam Team)은 mTeam 멤버 변수의 값을 설정하는 공개 함수
 	{
 		mTeam = Team;
 	}
 
 public:
 	void ChangeAnimation(EPlayerAnimType Type)
+//이 공개 함수는 EPlayerAnimType 타입의 Type 인자를 받아 플레이어의 애니메이션을 변경
 	{
 		mPlayerAnim->ChangeAnim(Type);
 	}
@@ -67,6 +71,9 @@ public:
 
 	// Called to bind functionality to input
 	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+	//플레이어 입력을 다룰 때 필요한 함수
+	//플레이어 입력에 기능을 바인딩하는 데 사용됨
+	//UInputComponent를 매개변수로 받는다.
 
 	virtual float TakeDamage(float DamageAmount,
 		FDamageEvent const& DamageEvent,
@@ -100,6 +107,44 @@ private:
 	void OverlapEnd(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, 
 		UPrimitiveComponent* OtherComp, int32 OtherBodyIndex);
 
+	/*
+	매개변수
+	BodyHit
+	->
+	HitComponent: 충돌이 발생한 원시(Primitive) 컴포넌트를 나타내는 포인터.
+	충돌이 발생한 객체의 컴포넌트
+
+	OtherActor: 상대적인 충돌이 발생한 다른 액터에 대한 포인터.
+	상대적으로 충돌한 다른 객체
+
+	OtherComp: 다른 액터의 충돌 컴포넌트를 나타내는 포인터.
+	NormalImpulse: 충돌에서의 정상적인 충격을 표현하는 FVector.
+	Hit: 충돌에 대한 정보를 포함하는 FHitResult 구조체
+	충돌에 대한 상세한 정보를 제공.
+
+	OverlapBegin
+	->이 함수는 두 객체가 겹칠 때 호출
+	겹침은 서로 교차하지만 물리적인 충돌은 발생하지 않는 상황
+	예를 들어, 플레이어가 아이템 영역에 진입하면 이 함수가 호출됨
+
+	OverlappedComponent: 겹침이 시작한 원시/객체 컴포넌트를 나타내는 포인터.
+	OtherActor: 겹친 다른 액터에 대한 포인터.
+	OtherComp: 겹친 다른 액터의 컴포넌트를 나타내는 포인터.
+	OtherBodyIndex: 다른 액터의 몸체(바디) 인덱스.
+	bFromSweep: 겹치기가 스위핑 동작으로부터 발생했는지 여부를 나타내는 부울 값.
+	SweepResult: 겹치기의 결과를 포함하는 FHitResult 구조체.
+	겹침의 상세한 정보를 확인할 수 있다. 
+
+
+	OverlapEnd
+	->겹침이 해제될 때 호출
+	OverlappedComponent: 겹치기가 끝난 원시 컴포넌트를 나타내는 포인터.
+	OtherActor: 겹친 다른 액터에 대한 포인터.
+	OtherComp: 겹친 다른 액터의 컴포넌트를 나타내는 포인터.
+	OtherBodyIndex: 다른 액터의 몸체(바디) 인덱스.
+		
+	*/
+
 public:
 	virtual void Attack1();
 	virtual void Attack2();
@@ -107,6 +152,5 @@ public:
 	virtual void Attack3();
 	
 	//	void FootInteraction(bool Left);
-
 
 };
